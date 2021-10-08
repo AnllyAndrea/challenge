e40413ffed521c2bad114c785a7345a45878dd5b
Anlly <anllyandreavr@gmail.com>
2021-10-07 22:02:10 -0500

    commit

--- test/buscar-mutante/buscar-mutante.service.spec.ts
+++ test/buscar-mutante/buscar-mutante.service.spec.ts
@@ -1,15 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BuscarMutanteService } from '../../src/buscar-mutante/buscar-mutante.service';
 import { BuscarMutanteModule } from '../../src/buscar-mutante/buscar-mutante.module';
-import * as ChallengeSchema from '../../src/models/challenge.schema';
-
 
 describe('BuscarMutanteService', () => {
   let service: BuscarMutanteService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BuscarMutanteService, ChallengeSchema]
+      providers: [BuscarMutanteService]
     }).compile();
 
     service = module.get<BuscarMutanteService>(BuscarMutanteService);
@@ -21,6 +19,4 @@ describe('BuscarMutanteService', () => {
       expect(service.buscarFila(dna)).toBe(true);
     });
   });
-
-
 });
